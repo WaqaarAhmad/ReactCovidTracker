50845c1eecff6d73aeb8e118ad3398775234a1d4
Waqar Ahmad <waqaar.ahmad@yahoo.com>
2020-06-26 11:58:58 +0500

    first commit

---
+++ build/precache-manifest.bf65a6a83c752faea5cfdb3fe00febde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22e72b76fff5f5abb5c5fcb972b2437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46487545ba1c37d91ea6",
+    "url": "/static/js/2.2b2b402a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b2b402a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa993050607bd2c83fe",
+    "url": "/static/js/main.84c7e341.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
